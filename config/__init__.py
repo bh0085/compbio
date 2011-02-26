32fbfb29bbfe530e98e8c6e8261423156102c879
Benjamin Holmes <bh0085@gliese.csail.mit.edu>
2011-02-25 20:44:20 -0500

    random

--- config/__init__.py
+++ config/__init__.py
@@ -8,32 +8,28 @@ def absPath(localPath):
 
 def dataPath(url):
   if url.count(':') == 0:
-    host_name = socket.gethostname()
-    volume_name = '/'
+    host_name = 'localhost'
+    volume_name = 'cb'
     localpath = url
   elif url.count(':') == 2:
     host_name = url.split(':')[0]
     volume_name = url.split(':')[1]
     localpath = url.split(':')[2]
   
-  if volume_name != '' and volume_name != '/':
-    volume_prefix = os.path.join('/Volumes/',volume_name)
-  else:
-    volume_prefix = '/'
-
+  if volume_name == '/':volume_prefix = '/'
+  elif volume_name == 'cb':volume_prefix = globals()['root']
+  elif volume_name == '~':volum_prefix = os.environ['HOME']
+  else: volume_prefix = os.path.join('/Volumes', volume_name)
   assert host_name == socket.gethostname() or \
       host_name == 'localhost'
-
   return os.path.join(os.path.join(volume_prefix,'data'), localpath)
 
-def dataURL(localpath, volume_name = '/',  host_name = None):
-  if host_name == None:
-    host_name = socket.gethostname()
-  assert host_name == socket.gethostname()
+def dataURL(localpath, volume_name = 'cb',  host_name = 'localhost'):
   return ':'.join([host_name, volume_name, localpath]) 
 
-def sqlitePath(dbname):
-  return 'sqlite:////'+os.path.join(globals()['root'], 'data/dbs/'+dbname+'.sqlite')
+def sqlitePath(dbname, **kwargs):
+  url = dataURL(os.path.join('dbs',dbname+'.sqlite'),**kwargs)
+  return 'sqlite:///'+dataPath(url)
 def postgresDefault(host = None):
   if host == 'broad':
     return 'postgres://benh@node1386.broadinstitute.org/benh'
---
+++ scripts/process_stks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+import sys, inspect, os, pickle
+from Bio import AlignIO
+
+os.chdir(os.path.dirname(inspect.stack()[0][1]))
+for inp, out in sys.argv[1:]:
+inp_dic = pickle.load(open(os.path.join('scr_inputs', inp)))
+filename = inp_dic['filename']
+align = AlignIO.parse(filename)
+r0 = align.next()
+
+fout = open(out, 'w')
+fout.write(r0.__str__())
+fout.close()
+
+exit()
