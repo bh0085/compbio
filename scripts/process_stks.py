20737097d9eb800343d6532b2e3da02dccd182b0
Benjamin Holmes <bh0085@gliese.csail.mit.edu>
2011-02-28 22:44:57 -0500

    messageless push on Mon Feb 28 22:44:57 EST 2011

--- scripts/process_stks.py
+++ scripts/process_stks.py
@@ -17,23 +17,23 @@ def make_params():
         inp_dicts.append({'file_url':file_url})
         print f
       
-  make_bsubs.make('process_stks.py', inp_dicts, mem_req = 400)
+  make_bsubs.make('process_stks.py', inp_dicts, mem_req = 2000)
 
 def main():
 
   name= sys.argv[1]
 
-  f0 = open(os.environ['HOME'] + '/hello.txt','w')
-  f0.write(name.__str__())
-  f0.close()
 
+  print 'getting input dictionary'
   inp_dic = pickle.load(open(config.scriptInputPath(name)))
   
   file_url = inp_dic['file_url']
+  print 'file_url: %s' % file_url
+  print 'parsing alignment'
   align = AlignIO.parse(open(config.dataPath(file_url)), 'stockholm')
+  print 'alignment parsed: reading record'
   r0 = align.next()
-  print 'hey whatsup'
-  
+  print 'record read, writing to the filesystem'
 
   fout = open(config.scriptOutputPath(name), 'w')
   fout.write(r0.__str__())
