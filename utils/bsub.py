0fb75b9a84675581bc0ea474411c54e50eccbb75
Benjamin Holmes <bh0085@gliese.csail.mit.edu>
2011-04-18 22:46:55 -0400

    messageless push on Mon Apr 18 22:46:55 EDT 2011

--- utils/bsub.py
+++ utils/bsub.py
@@ -34,7 +34,7 @@ class eyeball(object):
         
 
   def statii():
-    jobs = subprocess.Popen'bjobs '+ ' '.join(self.run_ids), shell = True, stdout = subprocess.PIPE).\
+    jobs = subprocess.Popen('bjobs '+ ' '.join(self.run_ids), shell = True, stdout = subprocess.PIPE).\
         communicate()[0]
     lines = '\n'.split(jobs)
     cols, lines = lines[0],lines[1:]
