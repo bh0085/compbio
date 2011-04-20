8886bf4e3741ca513ea821b3246c36eddf44d717
Benjamin Holmes <bh0085@gliese.csail.mit.edu>
2011-04-20 12:11:39 -0400

    messageless push on Wed Apr 20 12:11:39 EDT 2011

--- utils/bs_macros.py
+++ utils/bs_macros.py
@@ -1,3 +1,8 @@
+import subprocess as spc
+import compbio.utils.bsub as bsub
+import scipy.io as sio
+from numpy import *
+
 def runmat(script, input_dict, run_id):
     tmpnames = bsub.mat_tmp_fnames(run_id,2)
     sio.savemat(tmpnames[0],input_dict)
