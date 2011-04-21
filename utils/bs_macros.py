0d0d0ddf0416c129f45c2de56b4936410488136f
Benjamin Holmes <bh0085@gliese.local>
2011-04-21 16:07:46 -0400

    messageless push on Thu Apr 21 16:07:46 EDT 2011

--- utils/bs_macros.py
+++ utils/bs_macros.py
@@ -6,7 +6,8 @@ from numpy import *
 def runmat(script, input_dict, run_id):
     tmpnames = bsub.mat_tmp_fnames(run_id,2)
     sio.savemat(tmpnames[0],input_dict)
-    cstr = '''matlab -nodesktop -nosplash -r "{2}('{0}', '{1}'''.\
+
+    cstr = '''matlab -nodesktop -nosplash -r "{2}('{0}', '{1}' )"'''.\
         format(*(tmpnames+[script]) )
     sub = spc.Popen(cstr,shell = True, stdout = spc.PIPE).\
         communicate()[0]
