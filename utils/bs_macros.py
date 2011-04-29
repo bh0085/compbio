56efe737be9bb43146ff5f5310b2d4c4d16292a2
Benjamin Holmes <bh0085@gliese.csail.mit.edu>
2011-04-29 00:23:43 -0400

    messageless push on Fri Apr 29 00:23:43 EDT 2011

--- utils/bs_macros.py
+++ utils/bs_macros.py
@@ -6,8 +6,8 @@ from numpy import *
 def runmat(script, input_dict, run_id):
     tmpnames = bsub.mat_tmp_fnames(run_id,2)
     sio.savemat(tmpnames[0],input_dict)
-    cstr = '''matlab -nodesktop -nosplash -r "{2}('{0}', '{1}' ); exit"'''.format(*(tmpnames + [script]))
-    sxs = spc.Popen(cstr, shell = True, stdout = spc.PIPE).communicate()
+    cstr = '''matlab -nojvm -nodisplay -nosplash -r "{2}('{0}', '{1}' ); exit"'''.format(*(tmpnames + [script]))
+    sxs = spc.Popen(cstr, shell = True).communicate()
     output = sio.loadmat(tmpnames[1])
     o00 = output['out_struct'][0][0]
     dout   = dict([(k, o00.__getattribute__(k)) for k in o00._fieldnames])
