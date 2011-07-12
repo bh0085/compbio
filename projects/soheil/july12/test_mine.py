17061d759d4f4c23869cf8c7d80f71e91bfd6220
Benjamin Holmes <bh0085@gliese.local>
2011-07-12 14:39:43 -0400

    messageless push on Tue Jul 12 14:39:43 EDT 2011

--- projects/soheil/july12/test_mine.py
+++ projects/soheil/july12/test_mine.py
@@ -7,8 +7,8 @@ matfiles = [f for f in os.listdir(sdp) if '.mat' in f]
 for f in matfiles: 
     infile, resfile, totfile = \
         f, \
-        os.path.join(*( os.path.split(f)[:-1]+['mat_out']+os.path.split(f)[-1:])),\
-        os.path.join(*( os.path.split(f)[:-1]+['mat_all_out']+os.path.split(f)[-1]))
+        os.path.join(*( os.path.split(f)[:-1]+('mat_out',)+os.path.split(f)[-1:])),\
+        os.path.join(*( os.path.split(f)[:-1]+('mat_all_out',)+os.path.split(f)[-1]))
     print infile, resfile, totfile
     
 
