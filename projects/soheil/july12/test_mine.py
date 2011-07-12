0d26cee0327eec0737f52300b633ef1212a2c792
Benjamin Holmes <bh0085@gliese.local>
2011-07-12 14:38:59 -0400

    messageless push on Tue Jul 12 14:38:59 EDT 2011

--- projects/soheil/july12/test_mine.py
+++ projects/soheil/july12/test_mine.py
@@ -5,6 +5,11 @@ sdp = cfg.dataPath('soheil')
 matfiles = [f for f in os.listdir(sdp) if '.mat' in f]
 
 for f in matfiles: 
-    print f
+    infile, resfile, totfile = \
+        f, \
+        os.path.join(*( os.path.split(f)[:-1]+['mat_out']+os.path.split(f)[-1:])),\
+        os.path.join(*( os.path.split(f)[:-1]+['mat_all_out']+os.path.split(f)[-1]))
+    print infile, resfile, totfile
+    
 
 exit
