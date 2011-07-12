220f75ad721270a2cb3df99d2d769f8f2148414a
Benjamin Holmes <bh0085@gliese.local>
2011-07-12 14:40:16 -0400

    messageless push on Tue Jul 12 14:40:16 EDT 2011

--- projects/soheil/july12/test_mine.py
+++ projects/soheil/july12/test_mine.py
@@ -8,7 +8,7 @@ for f in matfiles:
     infile, resfile, totfile = \
         f, \
         os.path.join(*( os.path.split(f)[:-1]+('mat_out',)+os.path.split(f)[-1:])),\
-        os.path.join(*( os.path.split(f)[:-1]+('mat_all_out',)+os.path.split(f)[-1]))
+        os.path.join(*( os.path.split(f)[:-1]+('mat_all_out',)+os.path.split(f)[-1:]))
     print infile, resfile, totfile
     
 
