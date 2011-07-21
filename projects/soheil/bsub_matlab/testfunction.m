a0f3baa0bb5891582a1e86731f29f60c26cab0e0
Benjamin Holmes <bh0085@gliese.csail.mit.edu>
2011-07-21 17:31:07 -0400

    messageless push on Thu Jul 21 17:31:07 EDT 2011

--- projects/soheil/bsub_matlab/testfunction.m
+++ projects/soheil/bsub_matlab/testfunction.m
@@ -1,3 +1,4 @@
 function testfunction(input_file, output_file)
+input_vars = load(input_file)
 hi = 'hi'
-save(output_file, 'hi')
+save(output_file, 'input_vars')
---
+++ projects/soheil/july12/testfunction.m
@@ -0,0 +1,3 @@
+function testfunction(input_file, output_file)
+hi = 'hi'
+save(output_file, 'hi')
