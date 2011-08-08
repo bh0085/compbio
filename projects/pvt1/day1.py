9533476aefc6512b844e1a5f730a2174d653e75c
Benjamin Holmes <bh0085@gliese.local>
2011-08-08 02:41:26 -0400

    messageless push on Mon Aug  8 02:41:26 EDT 2011

--- projects/pvt1/day1.py
+++ projects/pvt1/day1.py
@@ -39,7 +39,7 @@ PVT1 region.
                       run_id = 'run1d_single_{0}_{1}_{2}specs={3}'.format(win_len,win_ofs,spec_count,b0),
                       spec_count = spec_count))
         #for testing purposes, only run a small number!
-        if len(p) > 40:
+        if len(p) > 20:
             break
 
     runid = 'run1d_{0}_{1}_{2}specs'.format(win_len,win_ofs,spec_count)
